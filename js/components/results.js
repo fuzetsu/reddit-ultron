4fc6788f3eb3701e51c75ee3cfb444a6ec22d504
Daniel Loomer <fuzetsu490@gmail.com>
2015-09-11 20:55:08 -0400

    move post logic to controller and add title gen
    moved logic to generate post content and title to controller and added
    some basic title displaying

--- js/components/results.js
+++ js/components/results.js
@@ -6,13 +6,45 @@ app.cmp.Results = {
       query: m.route.param('query'),
       backToSearch: function() {
         m.route('/search');
-      }
-    };
-    app.model.Reddit[args.type](m.route.param('query'))
-      .then(ctrl.posts)
-      .then(ctrl.loading.bind(null, false))
-      .then(m.redraw, function() {
-        // error case
+      },
+      genPostTitle: function(post) {
+        return m('div.post-info', [
+          m('div.title', post.title),
+          m('div.details', [
+            m('span.score', post.score),
+            ' upvotes and ',
+            m('a[target=_blank]', {
+              href: app.CONST.reddit + post.permalink
+            }, post.num_comments + ' comments'),
+            ' posted by ',
+            m('a[target=_blank]', {
+              href: app.CONST.reddit + '/user/' + post.author
+            }, '/u/' + post.author),
+            ctrl.isMultiReddit ? ([
+              ' to ',
+              m('a[target=_blank]', {
+                config: m.route,
+                href: '/r/' + post.subreddit
+              }, '/r/' + post.subreddit)
+            ]) : '',
+          ])
+        ]);
+      },
+      genPostContent: function(post) {
+        var content = [];
+        if(/\.(jpg|png|gif)$/i.test(post.url)) {
+          content = m('img', {onclick: mutil.toggleExpand, src: post.url});
+        } else if(/imgur\.com\/[a-z0-9]+$/i.test(post.url)) {
+          content = m('img', {onclick: mutil.toggleExpand, src: post.url + '.jpg'});
+        } else {
+          if(/\.(jpg|png|gif)$/i.test(post.thumbnail)) {
+            content.push(m('img', {src: post.thumbnail}));
+          }
+          content.push(m('a[target=_blank]', {href:post.url}, post.url));
+        }
+        return m('div.post-content', content);
+      },
+      showError: function() {
         ctrl.loading(false);
         Velocity(util.q('.error'), {
           opacity: 1,
@@ -21,7 +53,15 @@ app.cmp.Results = {
           display: 'block'
         });
         m.redraw();
-      });
+      }
+    };
+    // determine is this is a multireddit
+    ctrl.isMultiReddit = (args.type === 'search' || ctrl.query.indexOf('+') !== -1);
+    // request the results
+    app.model.Reddit[args.type](m.route.param('query'))
+      .then(ctrl.posts)
+      .then(ctrl.loading.bind(null, false))
+      .then(m.redraw, ctrl.showError);
     return ctrl;
   },
   view: function(ctrl, args) {
@@ -29,19 +69,14 @@ app.cmp.Results = {
       m('span.back-to-search', {
         onclick: ctrl.backToSearch
       }, [
-        m('i.fa.fa-arrow-left'),
+        mutil.icon('arrow-left'),
         ' Search'
       ]),
       m('div', ctrl.posts().map(function(post) {
-        var content;
-        if(/\.(jpg|png|gif)$/i.test(post.url)) {
-          content = m('img', {src: post.url});
-        } else if(/\.(jpg|png|gif)$/i.test(post.thumbnail)) {
-          content = m('img', {src: post.thumbnail});
-        } else {
-          content = m('span', post.url);
-        }
-        return m('div.post', content);
+        return m('div.post', [
+          ctrl.genPostTitle(post),
+          ctrl.genPostContent(post)
+        ]);
       })),
       m('img.loading', {
         src: app.IMAGES.loading,
